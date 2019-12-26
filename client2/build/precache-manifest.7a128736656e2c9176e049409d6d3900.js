477d8cf66037573898dc20fb6413c36239f5116b
guk1961 <guk1961@gmail.com>
2019-12-26 20:30:09 +0300

    add client2

---
+++ client2/build/precache-manifest.7a128736656e2c9176e049409d6d3900.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2d6edce2da53095ac08e643ec827b26b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ee9035882c156f4fa679",
+    "url": "/static/css/2.6a906a3b.chunk.css"
+  },
+  {
+    "revision": "c109077c323db5391fd5",
+    "url": "/static/css/main.e51b438d.chunk.css"
+  },
+  {
+    "revision": "ee9035882c156f4fa679",
+    "url": "/static/js/2.b6b3ead5.chunk.js"
+  },
+  {
+    "revision": "14c9108fd5a38194785d545908fb6cbd",
+    "url": "/static/js/2.b6b3ead5.chunk.js.LICENSE"
+  },
+  {
+    "revision": "c109077c323db5391fd5",
+    "url": "/static/js/main.5115d8c1.chunk.js"
+  },
+  {
+    "revision": "6b3defdcd169e5f902d5",
+    "url": "/static/js/runtime-main.1dc66141.js"
+  }
+]);
